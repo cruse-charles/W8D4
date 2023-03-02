633b98f729fd5def31a8c85ce9ebfab91bc85415
Charles Cruse <charle9@vt.edu>
2023-03-02 14:54:47 -0500

    finished constructor

--- clock.js
+++ clock.js
@@ -1,9 +1,15 @@
 class Clock {
   constructor() {
     // 1. Create a Date object.
+    const currentTime = new Date();
     // 2. Store the hours, minutes, and seconds.
+    this.hours = currentTime.getHours();
+    this.minutes = currentTime.getMinutes();
+    this.seconds = currentTime.getSeconds();
     // 3. Call printTime.
+    this.printTime()
     // 4. Schedule the tick at 1 second intervals.
+    setInterval(this._tick.bind(this),1000);
   }
 
   printTime() {
